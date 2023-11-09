432ca33035ec2468de90dad3be471c20fcecf795
Toby Warn <tobywarn@outlook.com>
2023-11-09 18:38:16 +0000

    loading into model

--- model/ml_model/model.py
+++ model/ml_model/model.py
@@ -1,6 +1,7 @@
 #Toby Warn (20026345)
 
 #Import Libraries
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -12,10 +13,19 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import RobustScaler
 
-def load_csv():
-    data = pd.read_csv(r'C:\Users\Toby\Documents\GitHub\DSP\team_stats\2022\Offensive\stats_1.csv')
-    return data
+def load_csv(folder_path):
+    all_data = []
+    for subdir, _, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith('.csv'):
+                file_path = os.path.join(subdir, file)
+                data = pd.read_csv(file_path)
+                all_data.append(data)
+    combined_data = pd.concat(all_data, ignore_index=True)
+    return combined_data
 
-data = load_csv()
+folder_path = r'C:\Users\Toby\Documents\GitHub\DSP\team_stats'
 
-print (data)
+data = load_csv(folder_path)
+print(data)
+
