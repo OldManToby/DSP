56ec316ae47dd3a8fb73f953f6c6a7c169269146
Toby Warn <tobywarn@outlook.com>
2023-11-10 11:33:56 +0000

    model changes

--- model/ml_model/model.py
+++ model/ml_model/model.py
@@ -1,76 +1,40 @@
-#Toby Warn (20026345)
-
-#Import Libraries
-import os
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-from sklearn.preprocessing import StandardScaler, RobustScaler, LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
-#load in the data through subfolders within 'team_stats'
-def load_csv(folder_path):
-    all_data = []
-    for subdir, _, files in os.walk(folder_path):
-        for file in files:
-            if file.endswith('.csv'):
-                file_path = os.path.join(subdir, file)
-                data = pd.read_csv(file_path)
-                all_data.append(data)
-    combined_data = pd.concat(all_data, ignore_index=True)
-    return combined_data
-folder_path = r'C:\Users\Toby\Documents\GitHub\DSP\team_stats'
-data = load_csv(folder_path)
-print(data)
-print(data.isnull().sum())
+# Load data from offensive, defensive, and special teams models
+offensive_data = pd.read_csv('path_to_offensive_model_results.csv')
+defensive_data = pd.read_csv('path_to_defensive_model_results.csv')
+special_team_data = pd.read_csv('path_to_special_team_model_results.csv')
 
-# Handle missing values by filling with the mean of each column
-data.fillna(data.mean(), inplace=True)
+# Assuming 'Team' is a common column in all three datasets
+# Merge the datasets on the 'Team' column
+merged_data = pd.merge(offensive_data, defensive_data, on='Team')
+merged_data = pd.merge(merged_data, special_team_data, on='Team')
 
-# Drop rows containing any remaining missing values
-data.dropna(inplace=True)
+# Assuming 'Outcome' is your target variable
+# Convert the outcome to binary
+merged_data['Outcome'] = merged_data['Outcome'].map({'Win': 1, 'Loss': 0})
 
-# Encoding categorical variables (team names)
-label_encoder = LabelEncoder()
-data['Team'] = label_encoder.fit_transform(data['Team'])
+# Split the data into features (X) and target variable (y)
+X = merged_data.drop(columns=['Team', 'Outcome'])  # Adjust columns as needed
+y = merged_data['Outcome']
 
-# Feature Scaling
-scaler = StandardScaler()
-numerical_features = ['Att', 'Cmp', 'Cmp.1', 'Yds/Att', 'Yds', 'TD', 'INT', '1st', '1st%', 'Sck', 'Rush', 'YPC', 'Rush.1', 'Rush.2', 'FR', 'SFTY', '3rd', '3rd.1', '4th', '4th.1', 'Scrm', 'FF', 'FR.1', 'INT.1', 'INT.2', 'Lng', 'Pass', 'Rate', '20+', '40+', 'SckY', 'Rush.3', 'Rec', 'Yds/Rec', 'Rec.1', 'Rec.2', 'Rec.3', 'Rsh', 'Tot', '2-PT', 'FGM', 'FG', '1-19 >', '20-29 >', '30-39 >', '40-49 >', '50-59 >', '60+ >', 'FG.1', 'XPM', 'XP', 'KRet', 'PRet', 'KO', 'TB', 'TB.1', 'Ret', 'Ret.1', 'OSK', 'OSK.1', 'OOB', 'Avg', 'FC', 'FUM']
-data[numerical_features] = scaler.fit_transform(data[numerical_features])
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Check for Null Values
-print("Null Values Check:")
-print(data.isnull().sum())
+# Train a logistic regression model
+logreg_model = LogisticRegression()
+logreg_model.fit(X_train, y_train)
 
-# Check Encoded Categorical Values
-print("\nUnique Values in 'Team' after Encoding:")
-print(data['Team'].unique())
+# Make predictions on the test set
+y_pred = logreg_model.predict(X_test)
 
-# Check Scaled Numerical Features
-print("\nSummary Statistics of Scaled Numerical Features:")
-print(data[numerical_features].describe())
+# Evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy}")
 
-# Correlation Analysis
-correlation_matrix = data[numerical_features].corr()
-plt.figure(figsize=(12, 10))
-sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
-plt.title('Correlation Heatmap')
-plt.show()
-
-# Visualizing Data Distributions with KDE and Boxplots
-for feature in numerical_features:
-    plt.figure(figsize=(12, 6))
-    
-    # Histogram with Kernel Density Estimation (KDE)
-    sns.histplot(data[feature], kde=True, color='blue', bins=30)
-    
-    # Boxplot to identify outliers
-    sns.boxplot(x=data[feature], color='red', width=0.2)
-    
-    plt.title(f'Distribution of {feature}')
-    plt.show()
+# Now you can use this trained model to make predictions on new data
+# new_data = ...  # Load your new data
+# new_predictions = logreg_model.predict(new_data)
