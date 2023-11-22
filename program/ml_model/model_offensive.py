6e39bcbad2161a257274270996ed2a56f6b3f085
Toby Warn <tobywarn@outlook.com>
2023-11-22 11:15:45 +0000

    okay

--- program/ml_model/model_offensive.py
+++ program/ml_model/model_offensive.py
@@ -3,7 +3,11 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
 from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.feature_selection import SelectKBest, f_classif
 
 class OffensiveModel:
     def __init__(self, folder_path, away_team, home_team):
@@ -13,9 +17,11 @@ class OffensiveModel:
         self.selected_teams_data = None
         self.numerical_features = []
         self.scaler = StandardScaler()
+        self.model = LogisticRegression()  # You can change the model as needed
 
-        # Load and preprocess data
+        # Load, preprocess data, and train the model
         self.load_and_preprocess_data()
+        self.train_model()
 
     def load_csv_offensive(self):
         all_data = []
@@ -36,7 +42,6 @@ class OffensiveModel:
             print(f"OffensiveModel: Data loaded successfully for {self.away_team} and {self.home_team}")
         else:
             print(f"OffensiveModel: No data available for selected teams: {self.away_team} and {self.home_team}")
-
 
     def preprocess_data(self):
         if self.selected_teams_data is not None and not self.selected_teams_data.empty:
@@ -83,3 +88,39 @@ class OffensiveModel:
         # If no data is available, return None
         print(f"No data available for selected teams: {self.away_team} and {self.home_team}")
         return None
+
+    def train_model(self):
+        # Assuming 'Winning Percentage' is a feature in your data
+        self.selected_teams_data['Winning_Percentage_Diff'] = (
+            self.selected_teams_data['Home_Team_Winning_Percentage'] - self.selected_teams_data['Away_Team_Winning_Percentage']
+        )
+
+        # Assuming 'Win' is the outcome column in your data
+        self.selected_teams_data['Outcome'] = (self.selected_teams_data['Winning_Percentage_Diff'] > 0).astype(int)
+
+        # Features (X) and Target Variable (y)
+        X = self.selected_teams_data.drop(columns=['Outcome', 'Team'])  # Adjust columns as needed
+        y = self.selected_teams_data['Outcome']
+
+        # Feature Selection using SelectKBest
+        k_best = 10  # Number of top features to select
+        feature_selector = SelectKBest(f_classif, k=k_best)
+        X_selected = feature_selector.fit_transform(X, y)
+
+        # Get the selected features
+        selected_features = X.columns[feature_selector.get_support()]
+
+        # Train-Test Split on the selected features
+        X_train, X_test, y_train, y_test = train_test_split(X[selected_features], y, test_size=0.2, random_state=42)
+
+        # Train a logistic regression model
+        logreg_model = LogisticRegression()
+        logreg_model.fit(X_train, y_train)
+
+        # Make predictions on the test set
+        y_pred = logreg_model.predict(X_test)
+
+        # Evaluate the model
+        accuracy = accuracy_score(y_test, y_pred)
+        print(f"Accuracy: {accuracy}")
+
